2e78c1d71da2758c7333e04a974f28b13e0fdcb9
Askhad Arzybaev <aarzybaev@gmail.com>
2024-02-23 17:50:36 +0600

    created store file

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
+import {keyboardReducer} from '../containers/Keyboard/keyboardSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    keyboard: keyboardReducer
+  },
 });
-
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
